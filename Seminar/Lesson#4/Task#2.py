65f9d2a49ae04671ccbcaa31c498d5a49c4a7ac8
Islambek Nasirdinov <islambeknasirdinov@gmail.com>
2023-11-06 22:22:06 +0300

    Решение Домашных задание урока 5

--- Seminar/Lesson#4/Task#2.py
+++ Seminar/Lesson#4/Task#2.py
@@ -32,3 +32,18 @@
 
 
 # 19
+
+arr =[5, 8, 6, 4, 9, 2, 7, 3]
+arr_count = []
+for i in range(len(arr)):
+    arr_count.append(arr[i-2] + arr[i-1] + arr[i])
+print(max(arr_count))
+
+#         <----------Решение варианты ---------->
+# arr_count = list()
+# for i in range(len(arr) - 1):
+#     arr_count.append(arr[i - 1] + arr[i] + arr[i + 1])
+# arr_count.append(arr[-2] + arr[-1] + arr[0])
+
+# # Вывод максимальной урожайности, которую может собрать собирающий модуль
+# print(max(arr_count))
