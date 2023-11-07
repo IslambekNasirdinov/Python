94010874e0997bdc2cd5b8cd4cc0f81de16b8a91
Islambek Nasirdinov <islambeknasirdinov@gmail.com>
2023-11-07 22:32:19 +0300

    Решение Домашных задание урока 6

--- Seminar/Lesson#6/Task#1.py
+++ Seminar/Lesson#6/Task#1.py
@@ -14,7 +14,6 @@
 # max_number = 10
 # На выходе:
 
-
 # 1
 # 2
 # 3
@@ -27,3 +26,10 @@
 # 14
 # 16
 # 19
+
+list_1 = [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
+min_number = 0 
+max_number = 10
+for i in range(len(list_1)):
+    if min_number <= list_1[i] <= max_number:
+        print(i)
